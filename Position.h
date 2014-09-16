5a54968ba1c3c233385ed6111b64a79ddcbb89bc
Alexander Senior <aseniorcsi@gmail.com>
2014-09-16 20:41:53 +1000

    Started new Position branch to work on Position class.

---
+++ Position.h
@@ -0,0 +1,25 @@
+/*
+ * Position.h
+ *
+ *  Created on: 16 Sep 2014
+ *      Author: alex
+ */
+
+#ifndef POSITION_H_
+#define POSITION_H_
+
+struct Position{
+public:
+	int row;
+	int col;
+public:
+	Position(int row = 0, int col = 0);
+	Position(const Position & other);
+	Position & operator=(const Position & other);
+};
+
+
+
+
+
+#endif /* POSITION_H_ */
