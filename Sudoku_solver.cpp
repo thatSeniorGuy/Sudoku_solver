d160985f36923d901d2133f0765cd3e446064775
Alexander Senior <aseniorcsi@gmail.com>
2014-09-16 20:46:00 +1000

    Added (mostly empty) source file for main.

---
+++ Sudoku_solver.cpp
@@ -0,0 +1,16 @@
+/*
+ * Sudoku_solver.cpp
+ *
+ *  Created on: 16 Sep 2014
+ *      Author: alex
+ */
+#include <iostream>
+
+int main(){
+	std::cout << "Nothing to see yet!" << std::endl;
+
+	return 0;
+}
+
+
+
