f5e90c8ff814b4b47a1a7bba89674f82bf1712d9
Alexander Senior <aseniorcsi@gmail.com>
2014-09-20 23:02:51 +1000

    Filled in some methods of class Puzzle.

--- Puzzle.cpp
+++ Puzzle.cpp
@@ -7,12 +7,26 @@
 
 #include "Puzzle.h"
 
-Puzzle::Puzzle() {
-	// TODO Auto-generated constructor stub
+Puzzle::Puzzle() : squares_(nullptr), solved_(false),
+	numLeftToSolve_(puzzle_size*puzzle_size) {
+
+	squares_ = new Square[puzzle_size*puzzle_size];
+
+	try{
+		for(int i = 0; i < puzzle_size*puzzle_size; i++){
+			squares_[i].setRow(i/puzzle_size);
+			squares_[i].setCol(i%puzzle_size);
+		}
+	}
+	catch (std::out_of_range & e) {
+		// The for loop above shouldn't throw any exceptions, but ...
+		delete[] squares_;
+		throw e;
+	}
 
 }
 
 Puzzle::~Puzzle() {
-	// TODO Auto-generated destructor stub
+	delete[] squares_;
 }
 
--- Puzzle.h
+++ Puzzle.h
@@ -8,10 +8,53 @@
 #ifndef PUZZLE_H_
 #define PUZZLE_H_
 
+#include <iostream>
+#include <string>
+#include "Square.h"
+
+/** \class Puzzle
+ * //TODO
+ *
+ */
 class Puzzle {
 public:
+	/** \var puzzle_size
+	 *  \brief The size of the Sudoku puzzle.
+	 *
+	 */
+	static const int puzzle_size = 9;
+public:
+
+	/** @name Constructors and destructors */
+	/**@{*/
+	/**
+	 * \brief Default constructor for the class.
+	 *
+	 * This creates an empty Puzzle, with all of its Squares unset, solved_ set
+	 * to false and numLeftToSolve_ set to \ref puzzle_size * \ref puzzle_size.
+	 */
 	Puzzle();
+
+	Puzzle(std::string filename);
+
+	Puzzle(const Puzzle & other);
+
+	Puzzle & operator=(const Puzzle & other);
+
+
+	/**
+	 * \brief Deletes all the Squares created in either constructor.
+	 */
 	~Puzzle();
+	/**@}*/
+
+
+
+
+private:
+	Square * squares_;
+	bool solved_;
+	int numLeftToSolve_;
 };
 
 #endif /* PUZZLE_H_ */
