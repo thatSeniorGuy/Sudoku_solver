12498307be127e82354af6886f6a312d94e573dd
Alexander Senior <aseniorcsi@gmail.com>
2014-09-20 22:04:30 +1000

    Created Puzzle class.

---
+++ Puzzle.cpp
@@ -0,0 +1,18 @@
+/*
+ * Puzzle.cpp
+ *
+ *  Created on: 20 Sep 2014
+ *      Author: alex
+ */
+
+#include "Puzzle.h"
+
+Puzzle::Puzzle() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Puzzle::~Puzzle() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ Puzzle.h
@@ -0,0 +1,17 @@
+/*
+ * Puzzle.h
+ *
+ *  Created on: 20 Sep 2014
+ *      Author: alex
+ */
+
+#ifndef PUZZLE_H_
+#define PUZZLE_H_
+
+class Puzzle {
+public:
+	Puzzle();
+	~Puzzle();
+};
+
+#endif /* PUZZLE_H_ */
