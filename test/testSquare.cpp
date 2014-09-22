85a67d3efa1481e6440cb2b80d4b5ab344efed7a
Alexander Senior <aseniorcsi@gmail.com>
2014-09-22 22:01:29 +1000

    Finished test function for 'setter' c'tor.

--- test/testSquare.cpp
+++ test/testSquare.cpp
@@ -17,13 +17,16 @@ using std::endl;
 
 void testSquare();
 static void testDefaultConstructor();
+static void testSetterConstructor();
 
 
 void testSquare(){
 	cout << "\n***Testing class Square.***\n" << endl;
 
 	testDefaultConstructor();
+	testSetterConstructor();
 
+	cout << "*** All done! ***" << endl;
 }
 
 static void testDefaultConstructor(){
@@ -66,12 +69,56 @@ static void testDefaultConstructor(){
 
 	}
 
+	try{
+		cout << "Trying to create bad square (-1 col), expecting exception ..." << endl;
+		Square s(0,-1);
+		assert(false && "Did not get expected exception.");
+	}
+	catch(std::out_of_range & e){
+		cout << "Got exception." << endl;
+
+	}
+
+
+	cout << "Creating a Square at r=0, c=1." << endl;
+	Square s4(0,1);
+	cout << "No issue." << endl;
+
+	cout << "Creating a Square at r=0, c=7." << endl;
+	Square s5(0,7);
+	cout << "No issue." << endl;
+
+	cout << "Creating a Square at r=0, c=8." << endl;
+	Square s6(0,8);
+	cout << "No issue." << endl;
+
+	try{
+		cout << "Trying to create bad square (col=9), expecting exception ..." << endl;
+		Square s(0,9);
+		assert(false && "Did not get expected exception.");
+	}
+	catch(std::out_of_range & e){
+		cout << "Got exception." << endl;
+
+	}
+
+	try{
+		cout << "Trying to create square at (-1,-1), expecting exception ..." << endl;
+		Square s(-1,-1);
+		assert(false && "Did not get expected exception.");
+	}
+	catch(std::out_of_range & e){
+		cout << "Got exception." << endl;
+
+	}
+
 	// Test whether set and what possible values are.
 	cout << "Testing square for setness, expecting unset: " << endl;
-	if(s.isSet())
-		assert(false && "Square was set?");
-	else
-		cout << "Square was not set." << endl;
+
+	assert(!s.isSet() && "Square was set?");
+
+
+	cout << "Square was not set." << endl;
 
 	cout << "Possible values should have all integers from 1 to : "
 			<< Square::puzzle_size << endl;
@@ -89,6 +136,50 @@ static void testDefaultConstructor(){
 		assert(false && "Did not have expected set of values?");
 }
 
+static void testSetterConstructor(){
+	cout << "\n***Testing 'setter' constructor.***\n" << endl;
+
+	std::set<int> posValues;
+	std::set<int> valValues;
+	for(int i = -1; i<=Square::puzzle_size; ++i)
+		posValues.insert(i);
+	for(int i=0; i<=Square::puzzle_size; ++i)
+		valValues.insert(i);
+
+	for(auto colIt : posValues){
+		for(auto rowIt : posValues){
+			for(auto valIt: valValues){
+				int col = *colIt;
+				int row = *rowIt;
+				int val = *valIt;
+				 // Invalid positions or values  should an std::out_of_range
+				if(col < 0 ||
+						col >= Square::puzzle_size ||
+						row < 0 ||
+						row >= Square::puzzle_size ||
+						val < 1 ||
+						val > Square::puzzle_size){
+					try{
+						Square s(row, col, val);
+						assert(false && "Expected exception?");
+					}
+					catch(std::out_of_range & e){
+						// All good
+					}
+				}
+
+				Square s(row, col, val);
+				std::set<int> testSet({val});
+				assert(row==s.getRow() && "Square's row not set correctly?");
+				assert(col==s.getCol() && "Square's col not set correctly?");
+				assert(val==s.getValue() &&
+						"Square's value not set correctly?");
+				assert(s.isSet() && "Square is not set?");
+				assert(s.getPosition()==testSet &&
+						"possibleValues not set correctly?");
+			}
+		}
+	}
 
 
-
+}
