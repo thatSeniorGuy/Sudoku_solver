ba750ef444c90dd60ba866dfd4d0a0e81d8815ce
Alexander Senior <aseniorcsi@gmail.com>
2014-09-22 19:35:19 +1000

    Modified main() so it can call the test functions for Square.

--- src/Sudoku_solver.cpp
+++ src/Sudoku_solver.cpp
@@ -5,9 +5,16 @@
  *      Author: alex
  */
 #include <iostream>
+#include <string>
 
-int main(){
-	std::cout << "Nothing to see yet!" << std::endl;
+extern void testSquare();
+
+int main(int argc, char * argv[]){
+
+	if(argc==2 && std::string(argv[1])=="testSquare")
+		testSquare();
+	else
+		std::cout << "Nothing to see yet!" << std::endl;
 
 	return 0;
 }
