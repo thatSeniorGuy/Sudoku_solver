589b3e1ebe7781279e35369a9656d9f0df6bfdec
Alexander Senior <aseniorcsi@gmail.com>
2014-09-22 19:30:30 +1000

    Finish test function for default Square c'tor.

--- test/testSquare.cpp
+++ test/testSquare.cpp
@@ -10,6 +10,7 @@
 #include "Square.h"
 #include <iostream>
 #include <cassert>
+#include <stdexcept>
 
 using std::cout;
 using std::endl;
@@ -29,11 +30,63 @@ static void testDefaultConstructor(){
 	cout << "\n***Testing default Square c'tor.***\n" << endl;
 
 	// Cases for row/column: -1, 0, 1, 7, 8, 9
+	try{
+		cout << "Trying to create bad square (-1 row), expecting exception ..." << endl;
+		Square s(-1,0);
+		assert(false && "Did not get expected exception.");
+	}
+	catch(std::out_of_range & e){
+		cout << "Got exception." << endl;
 
+	}
+
+	cout << "Creating a Square at r=0, c=0." << endl;
+	Square s(0,0);
+	cout << "No issue." << endl;
+
+	cout << "Creating a Square at r=1, c=0." << endl;
+	Square s1(1,0);
+	cout << "No issue." << endl;
+
+	cout << "Creating a Square at r=7, c=0." << endl;
+	Square s2(7,0);
+	cout << "No issue." << endl;
+
+	cout << "Creating a Square at r=8, c=0." << endl;
+	Square s3(8,0);
+	cout << "No issue." << endl;
+
+	try{
+		cout << "Trying to create bad square (row=9), expecting exception ..." << endl;
+		Square s(9,0);
+		assert(false && "Did not get expected exception.");
+	}
+	catch(std::out_of_range & e){
+		cout << "Got exception." << endl;
+
+	}
 
 	// Test whether set and what possible values are.
+	cout << "Testing square for setness, expecting unset: " << endl;
+	if(s.isSet())
+		assert(false && "Square was set?");
+	else
+		cout << "Square was not set." << endl;
 
+	cout << "Possible values should have all integers from 1 to : "
+			<< Square::puzzle_size << endl;
 
+	std::set<int> vals;
+	for(int i = 1; i <= Square::puzzle_size; ++i) {
+		auto ret = vals.insert(i);
+		if(!ret.second)
+			assert(false && "Error in making set?");
+	}
+
+	if(vals == s.getPossibleValues())
+		cout << "Have them all" << endl;
+	else
+		assert(false && "Did not have expected set of values?");
 }
 
 
