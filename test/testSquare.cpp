9ab43c2311bc41776d52c226d8675f6bccc091b6
Alexander Senior <alexander.senior@monash.edu>
2014-09-22 12:01:51 +1000

    Added directory for test files, and started creating tests for class Square.

---
+++ test/testSquare.cpp
@@ -0,0 +1,41 @@
+/**
+ * \file testSquare.cpp
+ *
+ * Test code for class Square.
+ *
+ *  Created on: 22 Sep 2014
+ *      Author: Alexander Senior.
+ */
+
+#include "Square.h"
+#include <iostream>
+#include <cassert>
+
+using std::cout;
+using std::endl;
+
+void testSquare();
+static void testDefaultConstructor();
+
+
+void testSquare(){
+	cout << "\n***Testing class Square.***\n" << endl;
+
+	testDefaultConstructor();
+
+}
+
+static void testDefaultConstructor(){
+	cout << "\n***Testing default Square c'tor.***\n" << endl;
+
+	// Cases for row/column: -1, 0, 1, 7, 8, 9
+
+
+	// Test whether set and what possible values are.
+
+
+}
+
+
+
+
