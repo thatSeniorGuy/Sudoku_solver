5904a98c7be7f6976dcdeff1a2880342aed2c100
Alexander Senior <aseniorcsi@gmail.com>
2014-10-03 19:39:22 +1000

    Started testing Puzzle class.

--- src/Sudoku_solver.cpp
+++ src/Sudoku_solver.cpp
@@ -8,11 +8,14 @@
 #include <string>
 
 extern void testSquare();
+extern void testPuzzle();
 
 int main(int argc, char * argv[]){
 
 	if(argc==2 && std::string(argv[1])=="testSquare")
 		testSquare();
+	else if(argc == 2 && std::string(argv[1])=="testPuzzle")
+		testPuzzle();
 	else
 		std::cout << "Nothing to see yet!" << std::endl;
 
--- test/testPuzzle.cpp
+++ test/testPuzzle.cpp
@@ -5,6 +5,37 @@
  *      Author: Alexander Senior.
  */
 
+#include "Puzzle.h"
+#include <iostream>
+#include <cassert>
+
+using std::cout;
+using std::endl;
+
+void testPuzzle();
+static void testDefaultCtor();
+
+void testPuzzle(){
+	cout << "***Testing class Puzzle. ***\n" << endl;
+
+	testDefaultCtor();
+
+
+
+	cout << "\n***All done!***" << endl;
+}
+
+void testDefaultCtor(){
+	cout << "***Testing default c'tor.***\n" << endl;
+
+	Puzzle puzzle;
+	assert(puzzle.isSolved()==false && "A default puzzle is not solved?");
+	assert(puzzle.getNumLeftToSolve() == Puzzle::NUM_SQUARES &&
+			"A default puzzle does not have NUM_SQUARES left to solve?");
+
+
+	cout << "\n*** No problems!" << endl;
+}
 
 
 
